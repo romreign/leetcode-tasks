1726f965d80aaf96a4ab53b0f2d9c15689628547
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-09-03 20:33:11 +0300

    Time: 7 ms (70.81%) | Memory: 21.4 MB (17.97%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -3,11 +3,13 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int k = 0;
         for (auto i = 1; i < nums.size(); i++)
-            if (nums[k] != nums[i])
+        {
+            if (nums[i] != nums[k])
             {
                 k++;
                 nums[k] = nums[i];
             }
+        }
         return k + 1;
     }
 };
