c3590725d2697cd2bb5b2421e479136f613bac83
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-09-03 23:51:03 +0300

    Time: 71 ms (72.52%) | Memory: 71.3 MB (61.73%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        cin.tie(0); 
-        ios::sync_with_stdio(0);
-        for (auto i = 0; i < arr.size() - 1; i++)
+        vector<int> result(arr.size());
+        auto maximum = -1;
+        for (int i = result.size() - 1; i >= 0; i--)
         {
-            int maximum = 0;
-            for (auto j = 1 + i; j < arr.size(); j++)
-            {
-                if (arr[j] > maximum)
-                    maximum = arr[j];
-            }
-            arr[i] = maximum;
+            result[i] = maximum;
+            maximum = max(maximum, arr[i]);
         }
-        arr[arr.size() - 1] = -1;
-        return arr;
+        return result;
     }
 };
