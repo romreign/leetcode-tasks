6e8c132526a262013a6b7f4a65f91ef98692bf54
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-09-22 00:58:22 +0300

    Time: 7 ms (80.88%) | Memory: 19.6 MB (94.51%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-        int left = 0, maxDist = 0;
-        for (auto right = 0 ; right < seats.size(); right++)
+        int left = 0, maxDist = 0, size = seats.size();
+        for (auto right = 0 ; right < size; right++)
         {
             if (seats[left] == 0 && seats[right] == 1)
             {
@@ -15,8 +15,7 @@ public:
                 left = right;
             }
         }
-        int size = seats.size() - 1;
-        maxDist = max((size - left), maxDist);
+        maxDist = max((size - 1 - left), maxDist);
         return maxDist;    
     }
 };
