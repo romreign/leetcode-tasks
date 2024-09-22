aa4ab2c13f018b7e859324868ace010101a421f2
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-09-22 19:53:09 +0300

    Time: 6 ms (58.59%) | Memory: 17.9 MB (8.22%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,13 +11,23 @@
  */
 class Solution {
 public:
-    int depth(TreeNode* root)
-    {
+    int maxDepth(TreeNode* root) {
         if (!root)
             return 0;
-        return max(depth(root->left), depth(root->right)) + 1; 
-    }
-    int maxDepth(TreeNode* root) {
-        return depth(root);
+        auto maxD = 0;
+        stack<pair<TreeNode*, int>> s;
+        s.push({root, 1});
+        while (!s.empty())
+        {
+            auto [m_root, D] = s.top();
+            s.pop();
+            if (m_root)
+            {
+                s.push({m_root->left, D + 1});
+                s.push({m_root->right, D + 1});
+                maxD = max(maxD, D);
+            }
+        }
+        return maxD;
     }
 };
