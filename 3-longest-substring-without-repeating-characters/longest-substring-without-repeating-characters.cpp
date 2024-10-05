425a20162d4873ab62e1a680bb7a3b95cc5d7231
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-05 19:43:59 +0300

    Time: 19 ms (41.58%) | Memory: 16.5 MB (13.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> m_set;
-        auto left_border = 0, max_count = 0;
-        for (auto right_border = 0; right_border < s.length(); right_border++)
+        auto first = 0, maxD = 0;
+        set<char> Set;
+        for(auto second = 0; second < s.length(); second++)
         {
-            if (m_set.count(s[right_border]) == 0)
+            while (Set.count(s[second]))
             {
-                m_set.insert(s[right_border]);
-                max_count = max(max_count, right_border - left_border + 1);
+                Set.erase(s[first]);
+                first++;
             }
-            else
-            {
-                while(m_set.count(s[right_border]) != 0)
-                {
-                    m_set.erase(s[left_border]);
-                    left_border++;
-                }
-                m_set.insert(s[right_border]);
-            }
+            Set.insert(s[second]);
+            maxD = max(maxD, second - first + 1);
         }
-        return max_count;
+        return maxD;
     }
 };
+
