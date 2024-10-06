e563bd32d58fc589de39a54a4c49e9841809c9c8
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-06 19:31:06 +0300

    Time: 118 ms (35.85%) | Memory: 99.3 MB (64.40%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        bool isup = false, isdown = false;
+        bool isup = true, isdown = true;
         for (auto i = 0; i < nums.size() - 1; i++)
         {
-            if (nums[i] < nums[i + 1])
-                isup = true;
-            else if (nums[i] > nums[i + 1])
-                isdown = true;
+            if (nums[i] > nums[i + 1]) isup = false;
+            else if (nums[i] < nums[i + 1]) isdown = false;
         }
-        return ((isdown == isup && isup == false) || (isdown != isup)) ? true : false;
+        return isup || isdown;
     }
 };
