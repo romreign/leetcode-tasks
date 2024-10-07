22578e63cc4619d6268115eaf88e913786f7dada
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-08 00:42:17 +0300

    Time: 27 ms (33.83%) | Memory: 26.6 MB (5.25%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    void sorted(vector<vector<int>>& intervals)
-    {
-        for (auto i = 0; i < intervals.size(); i++)
-            for (auto j = 0; j < intervals.size() - 1 - i; j++)
-                if (intervals[j][0] > intervals[j + 1][0])
-                    swap(intervals[j], intervals[j + 1]);
-    }
-
     bool intersection(vector<int> first, vector<int> second)
     {
         return max(first[0], second[0]) <= min(first[1], second[1]);
@@ -20,7 +12,7 @@ public:
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
-        sorted(intervals);
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> result;
         result.push_back(intervals[0]);
 
