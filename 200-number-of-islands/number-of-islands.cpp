58b0d8ba62f9e14100895896a4119e7a825bddb3
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-14 01:11:22 +0300

    Time: 34 ms (29.03%) | Memory: 24.3 MB (15.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,15 +4,26 @@ public:
     void dfs(int x, int y, vector<vector<char>>& grid, int m, int n, vector<vector<int>>& visited) {
         vector<int> dx{-1, 0, 0, 1};
         vector<int> dy{0, -1, 1, 0};
+        stack<pair<int, int>> st;
 
         visited[x][y] = 1;
-        for (auto to = 0; to < dx.size(); to++) {
-            auto tx = dx[to] + x;
-            auto ty = dy[to] + y;
-            if (tx >= 0 && ty >= 0 && tx < m && ty < n 
-               && !visited[tx][ty] && grid[tx][ty] == '1') {
-                dfs(tx, ty, grid, m, n, visited);
-               }
+        st.push({x, y});
+
+        while (!st.empty()) {
+            auto temp = st.top();
+            st.pop();
+            auto p1 = temp.first;
+            auto p2 = temp.second;
+            
+            for (auto i = 0; i < dx.size(); i++) {
+                auto tx = dx[i] + p1;
+                auto ty = dy[i] + p2;
+                if (tx >= 0 && ty >= 0 && tx < m && ty < n 
+                    && !visited[tx][ty] && grid[tx][ty] == '1') {
+                        st.push({tx, ty});
+                        visited[tx][ty] = 1;
+                    }
+            }
         }
     }
 
