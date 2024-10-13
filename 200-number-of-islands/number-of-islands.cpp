8775d6ea3610ca80520912616cbbd05401a574bf
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-14 01:01:13 +0300

    Time: 61 ms (13.30%) | Memory: 24 MB (20.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,28 +1,34 @@
 class Solution {
-    vector<vector<int>> visited;
 public:
-    void dfs(vector<vector<char>>& grid, int x, int y) {
+
+    void dfs(int x, int y, vector<vector<char>>& grid, int m, int n, vector<vector<int>>& visited) {
+        vector<int> dx{-1, 0, 0, 1};
+        vector<int> dy{0, -1, 1, 0};
+
         visited[x][y] = 1;
-        static vector<int> dx = {0, 1, 0, -1}; 
-        static vector<int> dy = {-1, 0, 1, 0};
-        for (auto i = 0; i < dx.size(); i++) {
-            auto tx = x + dx[i];
-            auto ty = y + dy[i];
-            if (tx >= 0 && tx < grid.size() && ty >= 0 && ty < grid[0].size() && grid[tx][ty] == '1' && !visited[tx][ty])
-                dfs(grid, tx, ty);
+        for (auto to = 0; to < dx.size(); to++) {
+            auto tx = dx[to] + x;
+            auto ty = dy[to] + y;
+            if (tx >= 0 && ty >= 0 && tx < m && ty < n 
+               && !visited[tx][ty] && grid[tx][ty] == '1') {
+                dfs(tx, ty, grid, m, n, visited);
+               }
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        auto count = 0;
-        auto n = grid.size(), m = grid[0].size();
-        visited = vector<vector<int>>(n, vector<int>(m));
-        for (auto i = 0; i < n; i++)
-            for (auto j = 0; j < m; j++)
+        auto m = grid.size(), n = grid[0].size();
+        int count = 0;
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+
+        for (auto i = 0; i < m; i++) {
+            for (auto j = 0; j < n; j++) {
                 if (!visited[i][j] && grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j);
+                    dfs(i, j, grid, m, n, visited);
                 }
+            }
+        }
         return count;
     }
 };
