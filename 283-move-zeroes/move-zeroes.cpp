bbca3ac080a496de349655db54a82bfb47970c64
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-15 22:32:06 +0300

    Time: 7 ms (98.62%) | Memory: 21.9 MB (26.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if (nums.size() < 2) return;
-        int first = 0, second = 0;
-        while (second != nums.size())
-        {
-            if (nums[first] == 0 && nums[second] != 0)
-                swap(nums[first], nums[second]);
-            if (nums[second] == 0 || second == first)
-                second++;
-            if (nums[first] != 0)
-                first++;   
-        }
+        auto slow = 0;
+        for (auto fast = 0; fast < nums.size(); fast++)
+            if (nums[fast]) {
+                swap(nums[slow], nums[fast]);
+                slow++;
+            }
     }
 };
