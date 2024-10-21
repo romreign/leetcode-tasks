7c52eb8b8fe7609092434ce2fc301978f334fe7b
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-21 15:18:59 +0300

    Time: 3 ms (87.54%) | Memory: 13.6 MB (32.75%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string result;
-        vector<pair<int,string>> vec = 
-        {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, 
-        {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-
-        for (auto i = 0; i < vec.size(); i++)
-        {
-            while (num >= vec[i].first)
-            {
-                num -= vec[i].first;
-                result += vec[i].second;
+        string result = "";
+        vector<pair<int,string>> aRoman 
+        {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, 
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, 
+        {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+        for (auto i = 0; i < aRoman.size(); i++){
+            while (num >= aRoman[i].first) {
+                num -= aRoman[i].first;
+                result += aRoman[i].second;
             }
-        }        
+        }
         return result;
     }
 };
