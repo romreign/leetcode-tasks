34c8e8762e744862d8f31ab74c312b130ce78b14
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-21 15:26:05 +0300

    Time: 4 ms (85.74%) | Memory: 14.3 MB (22.20%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int number = 0;
-        unordered_map<char, int> um
+        int result = 0;
+        map<char, int> mp
         {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-        for (auto i = 1; i < s.length(); i++)
-        {
-            if (um[s[i]] > um[s[i - 1]])
-                number -= um[s[i - 1]];
+        for (auto i = 1; i <= s.size(); i++) {
+            if (i < s.size() && mp[s[i]] > mp[s[i - 1]]) 
+                result -= mp[s[i - 1]];
             else
-                number += um[s[i - 1]];
+                result += mp[s[i - 1]];
         }
-        number += um[s[s.size() - 1]];
-        return number;
+        return result;
     }
 };
