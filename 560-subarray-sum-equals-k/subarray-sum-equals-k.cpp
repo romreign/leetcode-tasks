fd06033c0a7c66d39bd6a4ca9c0b58f089f6b96b
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-22 17:05:33 +0300

    Time: 32 ms (99.85%) | Memory: 45.4 MB (61.51%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,24 @@
+auto x = []() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+};
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0, curSum = 0;
-        unordered_map<int, int> um;
-        um[0] = 1;
-        for (auto i = 0; i < nums.size(); i++)
-        {
-            curSum += nums[i];
-            if (um.find(curSum - k) != um.end())
-                count += um[curSum - k];
-            um[curSum]++;
+        unordered_map<int, int> ht;
+        int count = 0, presum = 0;
+        ht[presum] = 1;
+        for (auto n : nums) {
+            presum += n;
+            auto it = ht.find(presum - k);
+            if (it != ht.end())
+                count += it->second;
+            ht[presum]++;
         }
         return count;
     }
 };
+   
