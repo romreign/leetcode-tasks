7c90e31e20c358365a0259b324094911093f4447
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-10-22 15:49:27 +0300

    Time: 0 ms (100.00%) | Memory: 8.9 MB (42.03%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+        vector<int> v(27);
+        for (auto c : s)
+            v[c - 'a']++;
+        for (auto c : t) {
+            if (v[c - 'a'] == 0)
+                return false;
+            v[c - 'a']--;
+        }
+        return true;
+    }
+};
