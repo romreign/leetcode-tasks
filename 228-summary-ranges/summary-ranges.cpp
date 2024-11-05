a062c1baf145554443b15ccf2c0dcc34bf39d21b
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-05 20:46:50 +0300

    Time: 0 ms (100.00%) | Memory: 8.6 MB (6.87%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<string> summaryRanges(const vector<int>& nums) {
-        if (!nums.size())
-            return {};
-
-        int l = 0;
+    vector<string> summaryRanges(vector<int>& nums) {
         vector<string> result;
-        for (auto r = 1; r < nums.size(); r++) {
-            if (nums[r] != nums[l] + 1 && l + 1 == r) {
-                result.push_back(to_string(nums[l]));
-                l = r;
+        int left = 0;
+        if (nums.size() == 0)
+            return {};
+            
+        for (auto right = 1; right < nums.size(); right++) {
+            if (left == right - 1 && nums[right] != nums[left] + 1) {
+                result.push_back(to_string(nums[left]));
+                left = right;
             }
-            else if (nums[r] != nums[r-1] + 1) {
-                result.push_back(to_string(nums[l]) + "->" + to_string(nums[r - 1]));
-                l = r;
+            else if (nums[right] != nums[right - 1] + 1) {
+                result.push_back(to_string(nums[left]) + "->" + to_string(nums[right - 1]));
+                left = right;
             }
         }
-        if (l != nums.size() - 1)
-            result.push_back(to_string(nums[l]) + "->" + to_string(nums[nums.size() - 1]));
+        if (left == nums.size() - 1)
+            result.push_back(to_string(nums[left]));
         else
-            result.push_back(to_string(nums[l]));
-
+            result.push_back(to_string(nums[left]) + "->" + to_string(nums[nums.size() - 1]));
         return result;
     }
 };
