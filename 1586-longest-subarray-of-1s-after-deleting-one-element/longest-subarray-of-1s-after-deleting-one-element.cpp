b4ee00b93ac27eb270faa58d443ab95377ddbe61
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-05 20:37:36 +0300

    Time: 0 ms (100.00%) | Memory: 58.7 MB (95.78%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int marr = -1, zero = 0, l = 0;
-        for (auto r = 0; r < nums.size(); r++) {
-            if (!nums[r])
-                zero++;
-            while (zero > 1) {
-                if (!nums[l])
-                    zero--;
-                l++;
+        int left = 0, countZeros = 0, maxD = 0;
+        for (auto right = 0; right < nums.size(); right++) {
+            if (nums[right] == 0)
+                countZeros++;
+
+            while (countZeros > 1) {
+                if (nums[left] == 0)
+                    countZeros--;
+                left++;
             }
-            marr = max(marr, r - l);
+            maxD = max(maxD, right - left);
         }
-        return marr;
+        return maxD;
     }
 };
