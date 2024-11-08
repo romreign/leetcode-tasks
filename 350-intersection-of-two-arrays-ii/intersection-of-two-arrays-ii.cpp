7cff897998f351bdc5b9f5492f2bea90a72ce90b
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-08 17:04:15 +0300

    Time: 0 ms (100.00%) | Memory: 14.8 MB (5.17%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_multiset<int> s1(nums1.begin(), nums1.end());
-        unordered_multiset<int> s2(nums2.begin(), nums2.end());
-        vector<int> result;
-        for (auto it = s1.begin(); it != s1.end(); it++)
-            if (s2.find(*it) != s2.end()){
+        std::vector<int> result;
+        unordered_multiset<int> m1 (nums1.begin(), nums1.end());
+        unordered_multiset<int> m2 (nums2.begin(), nums2.end());
+        for (auto it = m1.begin(); it != m1.end(); it++) {
+            if (m2.find(*it) != m2.end()) {
                 result.push_back(*it);
-                s2.erase(s2.find(*it));
+                m2.erase(m2.find(*it));
             }
+        }
         return result;
     }
 };
