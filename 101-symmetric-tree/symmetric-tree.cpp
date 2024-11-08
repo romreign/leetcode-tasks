ac45b674ad02ed82df1898d523f011b365309fb2
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-08 22:29:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (66.98%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,17 +11,17 @@
  */
 class Solution {
 public:
-    bool check(TreeNode* l, TreeNode* r)
-    {
+    bool symmetric (TreeNode* l, TreeNode* r) {
         if (!l || !r)
             return !l && !r;
-        if (l->val != r->val)
+        if (l->val == r->val)
+            return symmetric(l->left, r->right) && symmetric(l->right, r->left);
+        else
             return false;
-        return check(l->left, r->right) && check(l->right, r->left);
     }
     bool isSymmetric(TreeNode* root) {
         if (!root)
             return true;
-        return check(root->left, root->right);
+        return symmetric(root->left, root->right);
     }
 };
