a4ad99ab9b601addb58ea1b8f0baba23b1f72772
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-17 22:23:29 +0300

    Time: 0 ms (100.00%) | Memory: 10.6 MB (76.18%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int l = 0;
-        for (auto r = 0; r < nums.size(); r++) {
-            if (nums[r] != val) {
-                swap(nums[l], nums[r]);
-                l++;
+        int left = 0;
+        for (auto right = 0; right < nums.size(); right++) {
+            if (nums[right] != val) {
+                swap(nums[left], nums[right]);
+                left++;
             }
         }
-        return l;
+        return left;
     }
 };
