179fe065f155647da98c873e45b8b539b878582b
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-17 18:31:02 +0300

    Time: 0 ms (100.00%) | Memory: 96.2 MB (25.83%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, min_v = std::numeric_limits<int>::max();
+        int maxProf = 0, minB = prices[0];
         for (auto i = 0; i < prices.size(); i++) {
-            min_v = min(min_v, prices[i]);
-            profit = max(profit, prices[i] - min_v);
+            minB = min(minB, prices[i]);
+            maxProf = max(maxProf, prices[i] - minB);
         }
-        return profit;
+        return maxProf;
     }
 };
