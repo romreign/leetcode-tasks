ee21203e221400f3ea6b91a0f9e803f73e4f696a
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-17 18:46:33 +0300

    Time: 0 ms (100.00%) | Memory: 11.1 MB (97.58%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int first = m - 1, second = n - 1, index = m + n - 1;
-        while (second >= 0) {
-            if (first >= 0 && nums1[first] > nums2[second])
-                nums1[index--] = nums1[first--];
+        int index = m + n - 1;
+        m--, n--;
+        while (n >= 0) {
+            if (m >= 0 && nums1[m] > nums2[n])
+                nums1[index--] = nums1[m--];
             else
-                nums1[index--] = nums2[second--];
+                nums1[index--] = nums2[n--];
         }
     }
 };
