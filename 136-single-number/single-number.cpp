afd1fdbb65c396312ac864b2db700c25533c1c8f
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-11-17 22:25:06 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (97.41%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int number = 0;
-        for (auto n : nums)
-            number ^= n;
-        return number;
+        int x = 0;
+        for (auto num : nums)
+            x ^= num;
+        return x;
     }
 };
