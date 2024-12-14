88a6beab3151c5577a22e2aacd6ae0538539acfb
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-14 14:55:43 +0300

    Time: 0 ms (100.00%) | Memory: 14.9 MB (18.61%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int s = 0, n = mat.size();
-        for (auto i = 0; i < n; i++) 
-            s += mat[i][i] + mat[i][n - 1 - i];
-        if (n % 2 != 0)
-            s -= mat[n / 2][n / 2];
-        return s;
+        int sum = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            sum = sum + mat[i][i] + mat[i][mat.size() - 1 - i];
+            if (i == mat.size() - 1 - i)
+                sum = sum - mat[i][i];
+        }
+        return sum;
     }
 };
