5872ed6c7cb474f9aeff304213d7b2b7885f66c9
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-14 15:11:31 +0300

    Time: 3 ms (49.38%) | Memory: 9.5 MB (60.39%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,7 +3,7 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> mp1;
         unordered_map<char, char> mp2;
-        
+
         for (auto i = 0; i < s.size(); i++) {
             if (mp1.find(s[i]) != mp1.end() && mp1[s[i]] != t[i]) return false;
             if (mp2.find(t[i]) != mp2.end() && mp2[t[i]] != s[i]) return false;
