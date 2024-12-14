db4b8d3892406a1399aa6fee26ac3b10d3a48fa8
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-14 15:24:07 +0300

    Time: 4 ms (15.30%) | Memory: 14.9 MB (7.11%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
-        unordered_multiset<int> mp1 (nums1.begin(), nums1.end());
-        for (auto i = 0; i < nums2.size(); i++) {
-            if (mp1.find(nums2[i]) != mp1.end()) {
-                result.push_back(nums2[i]);
-                mp1.erase(mp1.find(nums2[i]));
+        unordered_multiset<int> mp2 (nums2.begin(), nums2.end());
+
+        for (auto n : nums1) {
+            if (mp2.find(n) != mp2.end()) {
+                result.push_back(n);
+                mp2.erase(mp2.find(n));
             }
         }
         return result;
