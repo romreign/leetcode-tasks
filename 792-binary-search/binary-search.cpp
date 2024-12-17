83a1e94a57c4da6d2bea0fc6422c929a0f1d0502
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-18 00:49:16 +0300

    Time: 0 ms (100.00%) | Memory: 31.2 MB (34.36%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    bool good(int k, int t) {
-        if (k <= t)
-            return true;
-        return false;
-    }
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size();
-        while (r - l > 1) {
-            int m = (l + r) / 2; 
-            if (good(nums[m], target))
-                l = m;
-            else
-                r = m;
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] < target)
+                l = mid + 1;
+            else if (nums[mid] > target)
+                r = mid - 1;
+            else if (nums[mid] == target)
+                return mid;
         }
-        return nums[l] == target? l : -1;
+        return -1;
     }
 };
