4c0d872b4bb44f9e3df18a627bcad69334198ed9
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-17 16:36:43 +0300

    Time: 0 ms (100.00%) | Memory: 9.7 MB (36.45%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -3,21 +3,16 @@ public:
     bool isPathCrossing(string path) {
         set<pair<int, int>> mp {{0, 0}};
         int x = 0, y = 0;
-
         for (auto c : path) {
-            if (c == 'N')
-                y++;
-            else if (c == 'S')
-                y--;
-            else if (c == 'E')
-                x++;
-            else 
-                x--;
+            if (c == 'N') y++;
+            else if (c == 'S') y--;
+            else if (c == 'E') x++;
+            else x--;
+
             if (mp.find({x, y}) != mp.end())
                 return true;
-            mp.insert({x, y});
+            mp.insert({x,y});
         }
         return false;
     }
 };
-
