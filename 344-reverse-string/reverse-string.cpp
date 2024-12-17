c118c8c1ecdeb3da8e56c75dd0871bfec0d481d8
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-17 16:47:50 +0300

    Time: 0 ms (100.00%) | Memory: 27.3 MB (19.81%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int l = 0, r = s.size() - 1;
-        while (l < r) 
-            swap(s[l++], s[r--]);
+        while (l < r) {
+            swap(s[l], s[r]);
+            l++, r--;
+        }
     }
 };
