04bd4f365c04b6a24ed8cd998c75e6475e2d9e1e
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-17 16:55:47 +0300

    Time: 2 ms (54.90%) | Memory: 15.5 MB (19.61%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_set<string> cities;
-
-        for (auto city : paths) {
-            cities.insert(city[0]);
-        }
-
-        for (auto city : paths) {
-            string to = city[1];
-            if (cities.find(to) == cities.end())
-                return to;
+        unordered_set<string> st;
+        for (auto c : paths)
+            st.insert(c[0]);
+        for (auto i = 0; i < paths.size(); i++){
+            string buf = paths[i][1];
+            if (st.find(buf) == st.end())
+                return buf;
         }
         return "";
     }
