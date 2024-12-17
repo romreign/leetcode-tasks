a969fc63a105ffe11c3107b021508e5037bb1309
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-17 16:50:43 +0300

    Time: 14 ms (16.40%) | Memory: 12.5 MB (72.10%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    map<char, int> mp
+ map<char, int> mp
         {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
     int romanToInt(string s) {
-        int result = 0;
+        int num = 0;
         for (auto i = 0; i < s.size(); i++) {
             if (mp[s[i]] < mp[s[i + 1]])
-                result -= mp[s[i]];
+                num -= mp[s[i]];
             else
-                result += mp[s[i]];
+                num += mp[s[i]];
         }
-        return result;
+        return num;
     }
 };
