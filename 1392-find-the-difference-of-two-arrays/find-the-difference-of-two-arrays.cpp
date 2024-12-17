d2289644f78bbb6bb3343baa061e47cb6fc5065d
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-17 17:49:23 +0300

    Time: 26 ms (50.17%) | Memory: 37 MB (45.37%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>> result(2);
-        unordered_set<int> mp1(nums1.begin(), nums1.end());
-        unordered_set<int> mp2(nums2.begin(), nums2.end());
+        unordered_set<int> mp1 (nums1.begin(), nums1.end());
+        unordered_set<int> mp2 (nums2.begin(), nums2.end());
 
-        for (auto it = mp1.begin(); it != mp1.end(); ++it) 
-            if (mp2.find(*it) == mp2.end())
-                result[0].push_back(*it);
+        for (auto n : mp1) {
+            if (mp2.find(n) == mp2.end()) 
+                result[0].push_back(n);
+        }
 
-        for (auto it = mp2.begin(); it != mp2.end(); ++it)
-            if (mp1.find(*it) == mp1.end())
-                result[1].push_back(*it);
+        for (auto n : mp2) {
+            if (mp1.find(n) == mp1.end()) 
+                result[1].push_back(n);
+        }
         return result;
     }
 };
