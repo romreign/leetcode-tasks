e30a23ce088c3a5b7f854b54a753e1b44a6af0b1
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2024-12-26 00:48:23 +0300

    Time: 0 ms (100.00%) | Memory: 65.1 MB (16.00%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -12,9 +12,9 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        if (!root) return 0;
+        if (!root) return 0; 
         if (root->val < low) return rangeSumBST(root->right, low, high);
         if (root->val > high) return rangeSumBST(root->left, low, high);
-        return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+        return root->val + rangeSumBST(root->right, low, high) + rangeSumBST(root->left, low, high);
     } 
 };
