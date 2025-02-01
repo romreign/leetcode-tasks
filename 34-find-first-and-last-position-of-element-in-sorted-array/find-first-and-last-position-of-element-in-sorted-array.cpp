ae02412a12e7590a47f84c0f0edde4dcc7db5cf6
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2025-02-02 01:18:56 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.65%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result;
+    int LeftBoundary(const vector<int>& nums, const int& target) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int ans = -1;
+
+         while(lo <= hi) {
+            const int mi = lo + (hi - lo) / 2;
+            if (nums[mi] < target)
+                lo = mi + 1;
+            else if (nums[mi] > target) 
+                hi = mi -1;
+            else {
+                ans = mi;
+                hi = mi - 1;
+            }
+        }
+        return ans;
+    }
+
+    int RightBoundary(const vector<int>& nums, const int& target) {
         int lo = 0;
         int hi = nums.size() - 1;
         int ans = -1;
@@ -14,31 +32,13 @@ public:
                 hi = mi -1;
             else {
                 ans = mi;
-                hi = mi - 1;
-            }
-        }
-        result.push_back(ans);
-
-        if (ans == -1) {
-            result.push_back(ans);
-            return result;
-        }
-        lo = ans;
-        hi = nums.size() - 1;
-
-        while(lo <= hi) {
-            const int mi = lo + (hi - lo) / 2;
-            if (nums[mi] < target)
-                lo = mi + 1;
-            else if (nums[mi] > target) 
-                hi = mi -1;
-            else {
-                ans = mi;
                 lo = mi + 1;
             }
         }
-        result.push_back(ans);
+        return ans;
+    }
 
-        return result;
+    vector<int> searchRange(const vector<int>& nums, const int& target) {
+        return vector<int>{LeftBoundary(nums, target), RightBoundary(nums, target)};
     }
 };
