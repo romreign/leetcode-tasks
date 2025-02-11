606f76c8cbe7195ee568abe5c9c65f68de86d928
Roman Kurgannikov <h3raDoreNa-Programmer@yandex.ru>
2025-02-11 18:53:50 +0300

    Time: 180 ms (5.00%) | Memory: 68.6 MB (5.25%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkVowels(char& c) {
         unordered_set<char> mp {'a', 'e', 'i', 'o', 'u'};
-        
+
         if (mp.contains(tolower(c)))
             return true;
         return false;
@@ -12,14 +12,9 @@ public:
         int lo = 0, hi = s.size() - 1;
 
         while (lo < hi) {
-            if (checkVowels(s[lo]) && checkVowels(s[hi])) 
-                swap(s[lo++], s[hi--]);
-            else if (!checkVowels(s[lo]) && !checkVowels(s[hi]))
-                lo++, hi--;
-            else if (checkVowels(s[lo]))
-                hi--;
-            else
-                lo++;
+            while(lo < hi && !checkVowels(s[lo])) ++lo;
+            while(lo < hi && !checkVowels(s[hi])) --hi;
+            swap(s[lo++], s[hi--]);
         }
         return s;
     }
