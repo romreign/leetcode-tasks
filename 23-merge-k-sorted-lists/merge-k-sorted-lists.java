fc4bfa7f00a76b4a32e620b51ba1358bc9c24c5a
Roman Kurgannikov <kurgannikov.r@yandex.ru>
2025-09-05 22:41:04 +0300

    Time: 4 ms (78.71%) | Memory: 44.4 MB (80.60%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -12,11 +12,9 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
         
-        for (ListNode node : lists) {
-            if (node != null) {
+        for (ListNode node : lists) 
+            if (node != null) 
                 minHeap.offer(node);
-            }
-        }
         
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
@@ -25,9 +23,8 @@ class Solution {
             ListNode minNode = minHeap.poll();
             curr.next = minNode;
             curr = curr.next;
-            if (minNode.next != null) {
+            if (minNode.next != null)
                 minHeap.offer(minNode.next);
-            }
         }
         
         return dummy.next;
