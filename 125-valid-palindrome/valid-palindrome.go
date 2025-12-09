aebfb6a35e3c1da7b03db37de5e6ef78f69b0f31
Roman Kurgannikov <kurgannikov.r@yandex.ru>
2025-12-09 23:13:04 +0300

    Time: 48 ms (13.15%) | Memory: 5.1 MB (48.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.go
+++ 125-valid-palindrome/valid-palindrome.go
@@ -1,9 +1,9 @@
 func isPalindrome(s string) bool {
     for first, second := 0, len(s) - 1; first < second; first, second = first + 1, second - 1 {
-        for first < second && !isValidChar(string(s[first])) {
+        for first < second && !isValidChar(rune(s[first])) {
             first++
         }
-        for first < second && !isValidChar(string(s[second])) {
+        for first < second && !isValidChar(rune(s[second])) {
             second--
         }
         if strings.ToLower(string(s[first])) != strings.ToLower(string(s[second])) {
@@ -14,8 +14,8 @@ func isPalindrome(s string) bool {
     return true
 }
 
-func isValidChar(ch string) bool {
-    if ch >= "A" && ch <= "Z" || ch >= "a" && ch <= "z" || ch >= "0" && ch <= "9" {
+func isValidChar(ch rune) bool {
+    if ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9' {
         return true
     }
     return false
