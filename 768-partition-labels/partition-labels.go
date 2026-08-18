d4795a036feadacadd369742a3516c1b2eccd309
Roman Kurgannikov <kurgannikov.r@yandex.ru>
2026-08-18 21:16:17 +0300

    Time: 0 ms (100.00%) | Memory: 4 MB (84.48%) - LeetSync

--- 768-partition-labels/partition-labels.go
+++ 768-partition-labels/partition-labels.go
@@ -7,14 +7,14 @@ func partitionLabels(s string) []int {
 
     currMaxW := 0
     res := make([]int, 0, 1)
-    l, r := -1, 0
+    l, r := 0, 0
 
     for r < len(s) {
         d := letterLastIdx[s[r] - 'a']
         currMaxW = max(currMaxW, d)
         if r == currMaxW {
-            res = append(res, r - l)
-            l = r
+            res = append(res, r - l + 1)
+            l = r + 1
         }
         r++
     }
