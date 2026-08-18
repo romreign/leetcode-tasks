ef5d05571183b1aa861a488ed621477685620ea2
Roman Kurgannikov <kurgannikov.r@yandex.ru>
2026-08-18 15:58:05 +0300

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.72%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,14 +1,15 @@
 func twoSum(numbers []int, target int) []int {
-    first, second := 0, len(numbers) - 1
-    for first < second {
-        sum := numbers[first] + numbers[second]
-        if sum > target {
-            second--
-        } else if sum < target {
-            first++
+    s, e := 0, len(numbers) - 1
+
+    for s < e {
+        if numbers[s] + numbers[e] > target {
+            e--
+        } else if numbers[s] + numbers[e] < target {
+            s++
         } else {
-            return []int{first + 1, second + 1}
+            return []int{s+1, e+1}
         }
     }
-    return []int{}
+
+    return []int{-1, -1}
 }
